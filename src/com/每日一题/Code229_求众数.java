2f481c8cfab289c9e2413701bba45a517978b853
hanshunli001 <hanshunli001@ke.com>
2021-10-22 14:21:40 +0800

    每日一题

---
+++ src/com/每日一题/Code229_求众数.java
@@ -0,0 +1,19 @@
+package com.每日一题;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Code229_求众数 {
+
+    public static void main(String[] args) {
+        int[] input = new int[]{1,2,3};
+        List<Integer> res = new Code229_求众数().majorityElement(input);
+
+        System.out.println(Arrays.toString(res.toArray()));
+
+    }
+
+    public List<Integer> majorityElement(int[] nums) {
+
+    }
+}
